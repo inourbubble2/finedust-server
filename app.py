21abc66959c1726985607acf2b792732d3a28a17
Nayeon Kim <inourbubble2@gmail.com>
2021-10-14 13:12:55 +0900

    feat: csv 파일 읽기 초기 세팅

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
+import csv
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    file = open('static/2010.csv')
+    reader = csv.reader(file)
+    lines = list(reader)
+    temp_str = str(lines[:10])
+    return temp_str
 
 
 if __name__ == '__main__':
